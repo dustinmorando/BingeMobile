2574949cf299a35ee87a794da25e80bc8e9e2057
Dustin Morando <dustinmorando@me.com>
2016-10-06 13:54:26 -0400

    first commit

--- BingeMobile/DetailViewController.swift
+++ BingeMobile/DetailViewController.swift
@@ -26,6 +26,7 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
+        print("first commit")
     }
 
     override func didReceiveMemoryWarning() {
